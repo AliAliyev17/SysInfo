f660388cef840d4415ec942a6d4e7f3747cc6879
AliAliyev17 <66472567+AliAliyev17@users.noreply.github.com>
2021-11-17 18:48:41 +0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    if args[1]=="brightness" {
+        let mut br_max = fs::read_to_string("/sys/class/backlight/intel_backlight/max_brightness").expect("we shat");
+        let mut br_actual = fs::read_to_string("/sys/class/backlight/intel_backlight/brightness").expect("we shat");
+        
+        br_max.pop(); br_actual.pop();
+        
+        let br_max = br_max.parse::<u32>().unwrap();
+        let br_actual = br_actual.parse::<u32>().unwrap();
+        println!("Brightness is {}%", br_actual*100/br_max);
+    
+    } else if args[1]=="battery" {
+        let mut bat_max = fs::read_to_string("/sys/class/power_supply/BAT1/charge_full").expect("fuck");
+        let mut bat_actual = fs::read_to_string("/sys/class/power_supply/BAT1/charge_now").expect("fuck");
+        
+        bat_max.pop(); bat_actual.pop();
+        
+        let bat_max = bat_max.parse::<u32>().unwrap();
+        let bat_actual = bat_actual.parse::<u32>().unwrap();
+        println!("Battery is {}%", 100*bat_actual/bat_max);
+    }
+}
