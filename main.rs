447e539f4f21181d5f06206e0c806fc9fefb86e0
AliAliyev17 <66472567+AliAliyev17@users.noreply.github.com>
2021-11-18 12:32:07 +0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,8 +5,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     if args[1]=="brightness" {
-        let mut br_max = fs::read_to_string("/sys/class/backlight/intel_backlight/max_brightness").expect("we shat");
-        let mut br_actual = fs::read_to_string("/sys/class/backlight/intel_backlight/brightness").expect("we shat");
+        let mut br_max = fs::read_to_string("/sys/class/backlight/intel_backlight/max_brightness").expect("Could not read file.");
+        let mut br_actual = fs::read_to_string("/sys/class/backlight/intel_backlight/brightness").expect("Could not read file.");
         
         br_max.pop(); br_actual.pop();
         
@@ -15,8 +15,8 @@ fn main() {
         println!("Brightness is {}%", br_actual*100/br_max);
     
     } else if args[1]=="battery" {
-        let mut bat_max = fs::read_to_string("/sys/class/power_supply/BAT1/charge_full").expect("fuck");
-        let mut bat_actual = fs::read_to_string("/sys/class/power_supply/BAT1/charge_now").expect("fuck");
+        let mut bat_max = fs::read_to_string("/sys/class/power_supply/BAT1/charge_full").expect("Could not read file.");
+        let mut bat_actual = fs::read_to_string("/sys/class/power_supply/BAT1/charge_now").expect("Could not read file.");
         
         bat_max.pop(); bat_actual.pop();
         
